09d3f1f11048442e994879aaf6ae023505036531
LiviuDragutan <liviu_dragutan@yahoo.com>
2023-10-11 16:13:06 +0300

    message

---
+++ pct1.js
@@ -0,0 +1,18 @@
+
+for (let i = 0; i < 11; i++){
+    console.log (i)
+}
+
+
+let i = 0;
+while (i < 11) {
+    console.log(i);
+    i++;
+}
+
+
+let x = 0;
+ do {
+    console.log(x);
+    x++;
+ } while (x < 11);
